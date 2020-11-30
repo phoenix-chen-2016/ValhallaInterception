105e47415aedcde8b1c1469d26f5303f043fadc7
Phoenix <fphoenix923@hotmail.com>
2020-11-30 16:32:59 +0800

    通過測試：取得物件是Proxy物件

--- ValhallaInterception/DynamicProxyInterceptionServiceProviderFactory.cs
+++ ValhallaInterception/DynamicProxyInterceptionServiceProviderFactory.cs
@@ -16,6 +16,27 @@ namespace Valhalla.Interception
 
 			foreach (var descriptor in containerBuilder)
 			{
+				if (descriptor.ImplementationType != null)
+				{
+					var warppedType = proxy.ProxyBuilder.CreateClassProxyType(
+						descriptor.ImplementationType,
+						descriptor.ImplementationType.GetInterfaces(),
+						ProxyGenerationOptions.Default);
+
+					warppedServices.Add(
+						ServiceDescriptor.Describe(
+							descriptor.ServiceType,
+							sp =>
+							{
+								return ActivatorUtilities.CreateInstance(
+									sp,
+									warppedType);
+							},
+							descriptor.Lifetime));
+
+					continue;
+				}
+
 				warppedServices.Add(descriptor);
 			}
 
